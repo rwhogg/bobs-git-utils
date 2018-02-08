4f84554cd29ee1964e56e884c810dc2367eceb84
Bob Wombat Hogg <rwhogg@linux.com>
2018-02-08 07:54:04 -0800

    Fix git-curbranch
    Signed-off-by: Bob Wombat Hogg <rwhogg@linux.com>

--- git-curbranch.rb
+++ git-curbranch.rb
@@ -7,13 +7,19 @@ def getBranchName
     branchname = `git branchname`.strip.split(" ")
     if branchname.length >= 4
         # Two possibilities: in the middle of a bisect or not attached to a branch at the moment
-        if branchname[3] == "bisect"
-            "bisect " + `git rev-parse HEAD`
+        if branchname[2] == "bisect"
+            "bisect-" + `git rev-parse HEAD`
         else
-            # (HEAD detached at remote/branch)
-            gitBranchPart = branchname[3]
-            gitBranchNamePart = gitBranchPart.split("/")[1]
-            gitBranchNamePart.chomp(")").strip
+            # (HEAD detached at <commit-ish>)
+            gitBranchPart = branchname[3].chomp(")").strip
+
+            # check for <remote>/<branch>
+            if gitBranchPart.include? "/"
+                gitBranchPart.split("/")[1]
+            else
+                # (HEAD detached at <commit>)
+                "commit-" + gitBranchPart
+            end
         end
     else
         # Checked out on a branch
